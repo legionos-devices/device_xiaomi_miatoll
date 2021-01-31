d7fa603320558df053993852ac238c0b36637a58
rajkale99 <kaleraj.rk@gmail.com>
2021-02-01 02:51:13 +0530

    miatoll:Bringup LegionOS

---
+++ legion_miatoll.mk
@@ -0,0 +1,26 @@
+#
+# Copyright (C) 2020 The LegionOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from miatoll  device
+$(call inherit-product, device/xiaomi/miatoll/device.mk)
+
+# Inherit some common legionOS stuff
+$(call inherit-product, vendor/legion/config/common_full_phone.mk)
+
+# Bootanimation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Device identifier
+PRODUCT_NAME := legion_miatoll
+PRODUCT_DEVICE := miatoll
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Miatoll
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Official
+LEGION_BUILD_TYPE := OFFICIAL
