bcfe26764ddf2699694fc6dde863145d4edaa77a
rajkale99 <kaleraj.rk@gmail.com>
2021-07-09 21:09:22 +0530

    miatoll: BringUp LegionOS
    Signed-off-by: rajkale99 <kaleraj.rk@gmail.com>

--- legion_miatoll.mk
+++ legion_miatoll.mk
@@ -1,16 +1,22 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2020 The LegionOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from miatoll device
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_miatoll
+# Inherit some common LegionOS stuff
+$(call inherit-product, vendor/legion/config/common_full_phone.mk)
+
+PRODUCT_NAME := legion_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := SM6250
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Official
+LEGION_BUILD_TYPE := OFFICIAL
